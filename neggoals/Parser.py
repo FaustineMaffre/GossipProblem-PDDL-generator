d3d1c5347ea39a5bccc017343556e7b457a01611
Faustine <faustine.maffre@gmail.com>
2016-03-30 18:20:07 +0200

    Slight modifications and documentation added in the parser for negative goals.

--- neggoals/Parser.py
+++ neggoals/Parser.py
@@ -1,54 +1,95 @@
 """ Parser for the negative goals.
-Allows to parse sets of the form, e.g., {i-j-k : i!=j and j!=k and i!=k} U
-{i,j : i<3 or j<3}, meaning that i should not know whether j knows the secret of
-k, for i, j, k distinct, and that i should not know the secret of j for either
-i = 1, 2 or j = 1, 2.
+Allows to parse sets of the form, e.g., {i-j-k : i!=j & j!=k & i!=k} U
+{i,j : i<3 & j<=3}, meaning that i should not know whether j knows the secret
+of k, for i, j, k distinct, and that i should not know the secret of j for
+either i = 1, 2 and j = 1, 2, 3.
 Also allows instantiated negative goals of the form {1-2-3, 1-3}, meaning that 1
 should not know whether 2 knows the secret of 3 and the secret of 3 (equivalent
-to {i-j-k : i==1 and j==2 and k==3} U {i-j : i==1 and j==3}).
+to {i-j-k : i=1 & j=2 & k=3} U {i-j : i=1 & j=3}).
 """
 
 from pypeg2 import *
 import re
 
+""" Description of the grammar of negative goals.
+Comp ::= = | != | <= | >= | < | >
+Int ::= <integer>
+AgtName ::= <lower-case letter> | AgtName<lower-case letter> | AgtName<digit>
+Cst ::= AgtName Comp AgtName | AgtName Comp Int
+Csts ::= Cst | Csts & Csts
+Agts ::= AgtName | Agts-Agts
+AgtsInst ::= Int | AgtsInst-AgtsInst
+AgtsInsts ::= AgtsInst | AgtsInsts, AgtsInsts
+Set ::= {Agts} | {Agts : Csts} | {AgtsInsts}
+Sets ::= Set | Sets U Sets
+"""
 
+
+""" Comparison operator.
+"""
 class Comp(str):
-    grammar = re.compile(r'==|!=|<|>')
+    grammar = re.compile(r'(=|!=|<=|>=|<|>)')
 
 
+""" Integer.
+"""
 class Int(int):
-    grammar = attr('nb', re.compile(r'\d'))
+    grammar = attr('nb', re.compile(r'[1-9]\d*'))
 
 
+""" Name of an agent: a lower case letter possibly followed by lower case
+letters and numbers.
+"""
 class AgtName(str):
     grammar = attr('name', re.compile(r'[a-z]([a-z]|[0-9])*'))
 
 
+""" Simple constraint: a comparison between two agents or an agent name and an
+integer.
+"""
 class Cst(List):
     grammar = AgtName, Comp, [AgtName, Int]
 
 
+""" Conjunction of constraints, separared by '&'.
+"""
 class Csts(List):
-    grammar = Cst, maybe_some(re.compile(r'and|or'), Cst)
+    grammar = csl(Cst, separator='&')
 
 
+""" Sequence of agents, separated by '-'.
+"""
 class Agts(List):
     grammar = csl(AgtName, separator='-')
 
 
+""" Sequence of 'instantiated' agents (that is, integers), separated by '-'.
+"""
 class AgtsInst(List):
     grammar = csl(Int, separator='-')
 
 
+""" Several sequences of instantiated agents, separated by ','.
+"""
+class AgtsInsts(List):
+    grammar = csl(AgtsInst, separator=',')
+
+
+""" Set: either agents, possibly followed by constraints (specified by ':'), or
+sequences of instantiated agents, separated by ','.
+"""
 class Set(List):
-    grammar = '{', [(Agts, optional(':', Csts)), csl(AgtsInst, separator=',')], '}'
+    grammar = '{', [(Agts, optional(':', Csts)), AgtsInsts], '}'
 
 
+""" Union of sets, separated by 'U'.
+"""
 class Sets(List):
-    grammar = Set, maybe_some("U", Set)
+    grammar = csl(Set, separator='U')
 
 
-test = '{i1-j-k : i1==1 and j<2} U {i-j: i!=j} U {i : i> 0}'
-#test = '{1-2-3, 1-2}'
-ast = parse(test, Sets)
-print(ast)
+# test1 = '{i1-j-k : i1>=1 & j<2} U {i-j: i!=j} U {i}'
+# test2 = '{1-10-6}'
+# test3 = test1 + ' U ' + test2
+# ast = parse(test3, Sets)
+# print(ast)
